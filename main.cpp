435292a32a9d1c864143b85f31928aa685326900
lavihayem <100god12@gmail.com>
2017-08-16 21:06:20 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x=0;
+    cout<<  " please enter integer number"<<endl;
+    cin>>x;
+
+    cout << x << endl;
+    float y=0.0;
+    cout<<  " please enter float number"<<endl;
+    cin>>y;
+    cout << y << endl;
+    //cin>>x;
+    cout<<  " please enter double number"<<endl;
+    double z=0.0;
+    cin>>z;
+    cout << z << endl;
+    return 0;
+}
+/*123456789123456789123456789123456789
+2147483647
+
+Process returned 0 (0x0)   execution time : 21.854 s
+Press any key to continue.
+*/
